a5abc4e467df2f8f5623a1d753136968daa5faa5
Chintan <chintan@wohlig.com>
2017-05-29 23:39:27 +0530

    ok

--- www/js/services.js
+++ www/js/services.js
@@ -1,7 +1,7 @@
-// var adminurl = "http://192.168.43.147:80/api/"; //local
+var adminurl = "http://wohlig.io:1337/api/"; //local
 
 // var adminurl = "http://104.198.28.29:80/api/"; //server
-var adminurl = "http://192.168.0.117:1337/api/"; //server
+// var adminurl = "http://192.168.0.117:1337/api/"; //server
 
 // var imgpath = adminurl + "uploadfile/getupload?file=";
 var imgurl = adminurl + "upload/";
@@ -163,7 +163,24 @@ angular.module('starter.services', [])
           }
         });
         return product.priceUsed;
+      },
+      getOtherProducts: function (product, quantity) {
+        $http({
+          url: adminurl + 'Product/getAllOtherProduct',
+          method: 'POST',
+          withCredentials: true,
+        }).then(function (data) {
+          callback(data);
+        });
+      },
+      saveOrderCheckout: function (product, quantity) {
+        $http({
+          url: adminurl + 'Order/saveOrderCheckout',
+          method: 'POST',
+          withCredentials: true,
+        }).then(function (data) {
+          callback(data);
+        });
       }
-
     };
   });
