224f15ddd6d1588496701f04c6c7719c31e74b65
Pranay Joshi <pranayj1449517@gmail.com>
2017-06-03 16:26:32 +0530

    chnages

--- www/js/app.js
+++ www/js/app.js
@@ -397,4 +397,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         ctrl.$parsers.push(inputValue);
       }
     };
-  });
+  })
+
+
+  .directive("limitTo", [function() {
+      return {
+          restrict: "A",
+          link: function(scope, elem, attrs) {
+              var limit = parseInt(attrs.limitTo);
+              angular.element(elem).on("keypress", function(e) {
+                  if (this.value.length == limit) e.preventDefault();
+              });
+          }
+      }
+  }]);
